091bb1b8c58c364675c7b9d7bb4287240b8bfe7e
Remco Tolsma <info@remcotolsma.nl>
2015-11-10 12:41:01 +0100

    composer install

--- src/GatewayIntegration.php
+++ src/GatewayIntegration.php
@@ -4,4 +4,16 @@ class Pronamic_WP_Pay_Gateways_Rabobank_IDealLite_GatewayIntegration {
 	public function __construct() {
 		$this->id = 'rabobank-ideal-lite';
 	}
+
+	public function get_config_factory_class() {
+		return 'Pronamic_WP_Pay_Gateways_IDealBasic_ConfigFactory';
+	}
+
+	public function get_config_class() {
+		return 'Pronamic_WP_Pay_Gateways_IDealBasic_Config';
+	}
+
+	public function get_gateway_class() {
+		return 'Pronamic_WP_Pay_Gateways_IDealBasic_Gateway';
+	}
 }
