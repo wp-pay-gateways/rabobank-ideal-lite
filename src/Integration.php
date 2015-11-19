7211a47af7c59341840f23a9188277c881b8cb06
Remco Tolsma <info@remcotolsma.nl>
2015-11-19 13:46:13 +0100

    Extend from iDEAL Basic library.

--- src/Integration.php
+++ src/Integration.php
@@ -1,22 +1,10 @@
 <?php
 
-class Pronamic_WP_Pay_Gateways_Rabobank_IDealLite_Integration {
+class Pronamic_WP_Pay_Gateways_Rabobank_IDealLite_Integration extends Pronamic_WP_Pay_Gateways_IDealBasic_AbstractIntegration {
 	public function __construct() {
 		$this->id         = 'rabobank-ideal-lite';
 		$this->name       = 'Rabobank - iDEAL Lite';
 		$this->provider   = 'rabobank';
 		$this->deprecated = true;
-	}
-
-	public function get_config_factory_class() {
-		return 'Pronamic_WP_Pay_Gateways_IDealBasic_ConfigFactory';
-	}
-
-	public function get_config_class() {
-		return 'Pronamic_WP_Pay_Gateways_IDealBasic_Config';
-	}
-
-	public function get_gateway_class() {
-		return 'Pronamic_WP_Pay_Gateways_IDealBasic_Gateway';
 	}
 }
